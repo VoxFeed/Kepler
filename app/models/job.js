876cd853111591beb0c7ee7b81d2fe775b679b96
Gus Ortiz <gustavo@voxfeed.com>
2014-08-11 17:29:43 -0500

    First version of keppler-jq

---
+++ app/models/job.js
@@ -0,0 +1,89 @@
+var Mongoose = require('mongoose'),
+  MD5        = require('MD5'),
+  Job;
+
+Mongoose.connect('mongodb://localhost/keppler-jq');
+
+JobSchema = new Mongoose.Schema({
+  namespace : String,
+  jobId     : String,
+  data      : {}
+});
+
+JobSchema.statics.get = function get(namespace, callback) {
+  this.findOneAndRemove({namespace: namespace}, function findOneAndRemoveHandler(error, job) {
+    var response = {};
+
+    if (error || job === null) {
+      response = {status: 'fail', message: 'not found'};
+    }
+    else if (job) {
+      response = {status: 'success', jobId: job.jobId, data: job.data};
+    }
+
+    callback(response);
+  });
+};
+
+JobSchema.statics.put = function put(namespace, data, callback) {
+  var jobId = null, 
+    date  = new Date(), 
+    randomString, job, res;
+
+  randomString = date.toISOString().split('').sort(function () {
+    return 0.5 - Math.random()
+  }).join();
+
+  jobId = MD5(namespace + JSON.stringify(data) + randomString);
+
+  job = new Job({
+    namespace : namespace,
+    jobId : jobId,
+    data  : data
+  });
+
+  job.save(function jobSaveHandler(error, job) {
+    var response = {};
+
+    if (error) {
+      response = {status: "error"}
+    }
+    else if (job && job.jobId === jobId) {
+      response = {status : "success", jobId: jobId};  
+    }
+
+    callback(response);
+  });
+};
+
+JobSchema.statics.removeOne = function removeOne(jobId, callback) {
+  this.findOneAndRemove({jobId : jobId}, function findOneAndRemoveHandler(error, job) {
+    var response = {};
+
+    if (error || job === null) {
+      response = {status : 'fail', message : 'not found'};
+    } else if (job && job.jobId === jobId) {
+      response = {status : 'success', jobId : job.jobId};
+    }
+
+    callback(response);
+  });
+};
+
+JobSchema.statics.removeAll = function removeAll(namespace, callback) {
+  var query = this.remove({namespace: 'query'}, function removeHandler(error, removedCount) {
+    var response = {};
+
+    if (error || removedCount === 0) {
+      response = {status : 'fail', message:  'not found'};
+    }
+    else if (removedCount && removedCount > 0) {
+      response = {status : 'success', count : removedCount};
+    }
+
+    callback(response);
+  });
+};
+
+Job = Mongoose.model('Job', JobSchema);
+module.exports = Job;
---
+++ server.js
@@ -0,0 +1,102 @@
+var express      = require('express'),
+    path         = require('path'),
+    favicon      = require('static-favicon'),
+    logger       = require('morgan'),
+    cookieParser = require('cookie-parser'),
+    net          = require('net'),
+    bodyParser   = require('body-parser'),
+    routes       = require('./routes/index'),
+    Job          = require('./app/models/job'),
+    app          = express(),
+    server;
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
+    });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+server = net.createServer(function jobQueueHandler(socket) {
+    socket.on('data', function (chunk) {
+        var jsonData = {status:'unknown'};
+
+        try {
+            jsonData = JSON.parse(chunk.toString());
+
+            switch (jsonData.action) {
+              case 'put':
+                Job.put(jsonData.namespace, jsonData.data, function putHandler(response) {
+                  console.log("Put:Success:", response);
+                  socket.write(JSON.stringify(response));
+                });
+                break;
+              case 'get':
+                Job.get(jsonData.namespace, function getHandler(response) {
+                  console.log("Get:Success:", response);
+                  socket.write(JSON.stringify(response));
+                });
+                break;
+              case 'remove':
+                Job.removeOne(jsonData.data.jobId, function removeHandler(response) {
+                  console.log("Remove:Success:", response);
+                  socket.write(JSON.stringify(response));
+                });
+                break;
+              case 'remove_all':
+                Job.removeAll(jsonData.namespace, function removeAllHandler(response) {
+                  console.log("RemoveAll:Success:", response);
+                  socket.write(JSON.stringify(response));
+                });
+            }
+        } catch(err) {
+            console.log(err);
+        }
+
+    });
+});
+
+server.listen(8200, function listenerHandler() {
+  console.log('server bound');
+});
+
+app.listen(8000);
+
+module.exports = app;
