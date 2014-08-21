bc9c9b2c0f33553a6f86b95446c3128e965cc4a4
Manuel de la Torre <manuelmhtr@gmail.com>
2014-08-21 09:41:46 -0500

    Added get multiple support

--- tests/test.js
+++ tests/test.js
@@ -6,14 +6,14 @@ var kp = new kp_client({
 
 // Testing UPDATE
 kp.put('NAMESPACE_1', {id : 1234567, user : 'user', foo : 'bar'}, function(error, data){
-    console.log('Testing UPDATE (N1)', error, data);
+    console.log('\n\nTesting UPDATE (N1)\n', error, data);
 
     kp.put('NAMESPACE_1', {id : 7654321, user : 'user', foo : 'bar'}, function(error, data){
-        console.log('Testing UPDATE (N1)', error, data);
+        console.log('\n\nTesting UPDATE (N1)\n', error, data);
 
         // Testing REMOVE_ALL
         kp.removeAll('NAMESPACE_1', function(error, data){
-            console.log('Testing REMOVE_ALL', error, data);
+            console.log('\n\nTesting REMOVE_ALL\n', error, data);
         });
 
     });
@@ -21,15 +21,15 @@ kp.put('NAMESPACE_1', {id : 1234567, user : 'user', foo : 'bar'}, function(error
 });
 
 kp.put('NAMESPACE_2', {id : 1234567, user : 'user', foo : 'bar'}, function(error, data){
-    console.log('Testing UPDATE (N2)', error, data);
+    console.log('\n\nTesting UPDATE (N2)\n', error, data);
 
     // Testing FETCH and REMOVE
     kp.fetch('NAMESPACE_2', function(error, data){
-        console.log('Testing FETCH', error, data);
+        console.log('\n\nTesting FETCH\n', error, data);
         if(!error){
 
             kp.remove(data.jobId, function(error, data){
-                console.log('Testing REMOVE', error, data);
+                console.log('\n\nTesting REMOVE\n', error, data);
             });
 
         }
@@ -38,11 +38,27 @@ kp.put('NAMESPACE_2', {id : 1234567, user : 'user', foo : 'bar'}, function(error
 });
 
 kp.put('NAMESPACE_3', {id : 1234567, user : 'user', foo : 'bar'}, function(error, data){
-    console.log('Testing UPDATE (N3)', error, data);
+    console.log('\n\nTesting UPDATE (N3)\n', error, data);
 
     // Testing GET
     kp.get('NAMESPACE_3', function(error, data){
-        console.log('Testing GET', error, data);
+        console.log('\n\nTesting GET\n', error, data);
+    });
+
+});
+
+// Testing GET MULTIPLE
+kp.put('NAMESPACE_4', {id : 1234567, user : 'user', foo : 'bar'}, function(error, data){
+    console.log('\n\nTesting UPDATE (N4)\n', error, data);
+
+    kp.put('NAMESPACE_4', {id : 7654321, user : 'user', foo : 'bar'}, function(error, data){
+        console.log('\n\nTesting UPDATE (N4)\n', error, data);
+
+        // Testing GET MULTIPLE
+        kp.getMultiple('NAMESPACE_4', 10, function(error, data){
+            console.log('\n\nTesting GET MULTIPLE\n', error, data);
+        });
+
     });
 
 });
